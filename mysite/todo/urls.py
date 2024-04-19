377a1b59de19198b3def38b1937e33bfd53bea14
Eve Wright <e_wright5@mail.fhsu.edu>
2024-04-19 00:02:00 -0500

    Added templates

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from .views import IndexView
 from . import views
 
+app_name = 'todo'
 urlpatterns = [
-    path("", views.tasks, name="tasks"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
 ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Task
 
 
-def tasks(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    model = Task
+    template_name = "todo/index.html"
+    context_object_name = "tasks"
+
+
+class DetailView(generic.DetailView):
+    model = Task
+    template_name = "todo/detail.html"
