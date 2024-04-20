01bc27e36ac9dc952fb67d42f7233fb8d1778199
Eve Wright <e_wright5@mail.fhsu.edu>
2024-04-19 23:01:14 -0500

    Added create and update views and templates

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -8,4 +8,6 @@ app_name = 'todo'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("create", views.Create.as_view(), name="create"),
+    path("update/<int:pk>/", views.Update.as_view(), name="update"),
 ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from .models import Task
-
+from django.urls import reverse_lazy
 
 class IndexView(generic.ListView):
     model = Task
@@ -14,3 +14,21 @@ class DetailView(generic.DetailView):
     model = Task
     template_name = "todo/detail.html"
     context_object_name = "task"
+
+
+class Create(generic.CreateView):
+    model = Task
+    template_name = "todo/create.html"
+    fields = ['title', 'description', 'category']
+    success_url = reverse_lazy("todo:index")
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user  # Set the created_by field
+        return super().form_valid(form)
+
+
+class Update(generic.UpdateView):
+    model = Task
+    template_name = "todo/update.html"
+    fields = ['title', 'description', 'category']
+    success_url = reverse_lazy("todo:index")
